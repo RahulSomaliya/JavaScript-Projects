fa697d01891e2a9a9554e14cccaf4edf6b2ce7ca
Rahul Somaliya <rahulsomaliya007@gmail.com>
2023-04-05 19:48:46 +0530

    Completed mapty!

--- 4. Workout tracker with Leaflet - Learing OOP/starter/script.js
+++ 4. Workout tracker with Leaflet - Learing OOP/starter/script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 // popup for adding new workout configuration
 const popupConfig = {
   maxWidth: 250,
@@ -17,11 +14,25 @@ const popupConfig = {
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat, lng]
     this.distance = distance; // in km
     this.duration = duration; // in min
+  }
+
+  _setDescription() {
+    // prettier-ignore
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
+
+  count() {
+    this.clicks++;
   }
 }
 
@@ -32,6 +43,7 @@ class Running extends Workout {
     super(coords, distance, duration);
     this.cadence = cadence;
     this.calcPace();
+    this._setDescription();
   }
 
   calcPace() {
@@ -44,15 +56,16 @@ class Running extends Workout {
 class Cycling extends Workout {
   type = 'cycling';
 
-  constructor(coords, distance, duration, elevationGain) {
+  constructor(coords, distance, duration, elevation) {
     super(coords, distance, duration);
-    this.elevationGain = elevationGain;
+    this.elevation = elevation;
     this.calcSpeed();
+    this._setDescription();
   }
 
   calcSpeed() {
     // defined in km/h
-    this.speed = this.distance / (this.duration * 60);
+    this.speed = this.distance / (this.duration / 60);
     return this.speed;
   }
 }
@@ -71,17 +84,25 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // it'll contain all the initiators, event listners, and app-data
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
   constructor() {
+    console.log(`💎 To reset the localstorage, call "app.reset()"`);
     this._getPosition();
+
+    // Get workouts from localStorage
+    this._getLocalStorage();
 
     // Adding form event listner on-enter click for saving workout
     form.addEventListener('submit', this._newWorkout.bind(this));
 
     // adding event listner for input type change
     inputType.addEventListener('change', this._toggleElevationField);
+
+    // adding event listner for containerWorkouts
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -100,7 +121,7 @@ class App {
     const { longitude: long, latitude: lat } = position.coords;
 
     // preparing map near the location
-    this.#map = L.map('map').setView([lat, long], 13);
+    this.#map = L.map('map').setView([lat, long], this.#mapZoomLevel);
 
     // setting tile theme through openstreetmap (I can use google map here too)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -109,19 +130,35 @@ class App {
     }).addTo(this.#map);
 
     // setting the default map marker
-    L.marker([lat, long])
-      .addTo(this.#map)
-      .bindPopup('This is your current location!')
-      .openPopup();
+    // L.marker([lat, long])
+    //   .addTo(this.#map)
+    //   .bindPopup('This is your current location!')
+    //   .openPopup();
 
     // handling clicks on map to add new workout
     this.#map.on('click', this._showForm.bind(this));
+
+    // render workouts from localstorage
+    this.#workouts.forEach(workout => this._renderWorkoutMarker(workout));
   }
 
   _showForm(mapEventPosition) {
     this.#mapEvent = mapEventPosition;
     form.classList.remove('hidden');
     inputDistance.focus();
+  }
+
+  _hideForm() {
+    // Empty inputs
+    inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+      inputCadence.value =
+        '';
+    form.style.display = 'none';
+    form.classList.add('hidden');
+
+    setTimeout(() => (form.style.display = 'grid'), 1000);
   }
 
   _toggleElevationField() {
@@ -177,22 +214,21 @@ class App {
 
     // Add new object to workout array
     this.#workouts.push(workout);
-    console.log(`💎 this.#workouts: `, this.#workouts);
 
     // Render workout map as marker // adding marker to the map
-    this.renderWorkoutMarker(workout);
+    this._renderWorkoutMarker(workout);
 
     // Render workout on the list
+    this._renderWorkout(workout);
 
     // Hide form + clear input fields // clearing all the input fields
-    inputDistance.value =
-      inputDuration.value =
-      inputElevation.value =
-      inputCadence.value =
-        '';
+    this._hideForm();
+
+    // Set local storage to all workouts
+    this._setLocalStorage();
   }
 
-  renderWorkoutMarker(workout) {
+  _renderWorkoutMarker(workout) {
     L.marker(workout.coords) // creates the marker
       .addTo(this.#map) // adds this to map
       .bindPopup(
@@ -201,9 +237,102 @@ class App {
           className: `${workout.type}-popup`,
         })
       ) // create and binds popup to the market
-      .setPopupContent(`${workout.distance}`)
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃' : '🚴'} ${workout.description}`
+      )
       .openPopup();
+  }
+
+  _renderWorkout(workout) {
+    const { type, id, distance, duration, description } = workout;
+
+    let html = `
+      <li class="workout workout--${type}" data-id="${id}">
+        <h2 class="workout__title">${description}</h2>
+        <div class="workout__details">
+          <span class="workout__icon">${type === 'running' ? '🏃' : '🚴'}</span>
+          <span class="workout__value">${distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${duration}</span>
+          <span class="workout__unit">min</span>
+        </div>`;
+
+    if (type === 'running') {
+      html += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>`;
+    }
+
+    if (type === `cycling`) {
+      html += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🚵🏻‍♀️</span>
+            <span class="workout__value">${workout.elevation}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>`;
+    }
+
+    // appending it to the DOM
+    form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(event) {
+    const workoutElement = event.target.closest('.workout');
+
+    if (!workoutElement) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutElement.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: { duration: 1 },
+    });
+
+    // using the public interface
+    // NOTE: working with OOP and localstorage has this issue;
+    // When storing class Instances (objects) to the localstorage by making it a string (JSON.stringify) and converting back when we reload the page, we're basically losing the prototype chain of that object. So, at the time of reload, we'd have to create new instances and basically mimic the entire process of creating the instance as it would've been created as a fresh one. This functionality is not added in this project, this is just a note to understand this - that keeping workout.count() active would work just fine untill the page has been refreshed (As there no longer will be the prototype chain we'd have made traditionally)
+    // workout.count();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const localWorkouts = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!localWorkouts) return;
+
+    this.#workouts = localWorkouts;
+    this.#workouts.forEach(workout => {
+      this._renderWorkout(workout);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
-const app = new App();
+const app = new App
